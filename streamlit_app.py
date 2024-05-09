57de7cf108c7ae93e5bed40fb0d42daaa1d87fa9
michal-malik-adastra <michal.malik@adastragrp.com>
2024-05-09 11:43:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
-# Write directly to the app
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
 st.title(":cup_with_straw Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie")
 
