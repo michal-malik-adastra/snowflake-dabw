0c2dace579c6bc49ea824cb742cf10981da58adb
michal-malik-adastra <michal.malik@adastragrp.com>
2024-05-09 11:44:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ values ('{ingredients_string}', '{name_on_order}')"""
         st.success(f"Your Smoothie is ordered, {name_on_order}")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
