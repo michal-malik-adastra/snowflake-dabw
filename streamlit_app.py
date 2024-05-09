1161b6fdf3662cbdf8e135da43b38de8ba9bba21
michal-malik-adastra <michal.malik@adastragrp.com>
2024-05-09 11:45:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,4 +25,4 @@ values ('{ingredients_string}', '{name_on_order}')"""
         st.success(f"Your Smoothie is ordered, {name_on_order}")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
